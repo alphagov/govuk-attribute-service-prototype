661db6c6869bbe8d2054b38d3b32d88a6c7a34f4
Michael Walker <mike@barrucadu.co.uk>
2020-07-29 09:57:12 +0100

    Implement GET /oidc/user_info
    A successful UserInfo response must include:
    
    - The subject identifier
    - Any of the standard claims
    - Any additional claims the token grants access to
    
    (see section 5.3.2. "Successful UserInfo Response")
    
    We don't return values for any of the standard claims, but the
    response is still valid.

--- app/controllers/oidc/user_info_controller.rb
+++ app/controllers/oidc/user_info_controller.rb
@@ -1,5 +1,26 @@
 class Oidc::UserInfoController < ApplicationController
   before_action :authenticate_token!
 
-  def show; end
+  def show
+    oidc_response = { sub: pairwise_subject_identifier }
+
+    render json: Claim
+             .where(subject_identifier: subject_identifier)
+             .select { |c| Permissions.any_of_scopes_can_read(c.claim_identifier, token_scopes) }
+             .each_with_object(oidc_response) { |c, hsh| hsh[c.claim_identifier] = c.claim_value }
+  end
+
+private
+
+  def subject_identifier
+    @token[:true_subject_identifier]
+  end
+
+  def pairwise_subject_identifier
+    @token[:pairwise_subject_identifier]
+  end
+
+  def token_scopes
+    @token[:scopes]
+  end
 end
--- spec/requests/oidc/user_info_request_spec.rb
+++ spec/requests/oidc/user_info_request_spec.rb
@@ -1,3 +1,4 @@
+require "permissions"
 require "rails_helper"
 
 RSpec.describe "Oidc::UserInfo", type: :request do
@@ -10,12 +11,14 @@ RSpec.describe "Oidc::UserInfo", type: :request do
 
   let(:headers) { { accept: "application/json", authorization: "Bearer #{token}" } }
 
-  let(:token_json) do
+  let(:token_scopes) { %w[test_scope_1 test_scope_2] }
+
+  let(:token_hash) do
     {
-      true_subject_identifier: 42,
-      pairwise_subject_identifier: "aaabbbccc",
-      scopes: %w[scope1 scope2],
-    }.to_json
+      true_subject_identifier: "true-subject-identifier",
+      pairwise_subject_identifier: "pairwise-subject-identifier",
+      scopes: token_scopes,
+    }
   end
 
   describe "GET" do
@@ -23,12 +26,49 @@ RSpec.describe "Oidc::UserInfo", type: :request do
       before do
         stub_request(:get, "https://account-manager/api/v1/deanonymise-token?token=#{token}")
           .with(headers: { accept: "application/json", authorization: "Bearer account-manager-token" })
-          .to_return(body: token_json)
+          .to_return(body: token_hash.to_json)
       end
 
       it "returns 200" do
         get "/oidc/user_info", headers: headers
         expect(response).to be_successful
+      end
+
+      it "includes the pairwise subject identifier" do
+        get "/oidc/user_info", headers: headers
+        expect(JSON.parse(response.body)).to include("sub" => token_hash[:pairwise_subject_identifier])
+      end
+
+      it "does not include the true subject identifier" do
+        get "/oidc/user_info", headers: headers
+        expect(response.body).to_not include(token_hash[:true_subject_identifier])
+      end
+
+      context "a claim exists" do
+        let!(:claim) do
+          FactoryBot.create(
+            :claim,
+            subject_identifier: token_hash[:true_subject_identifier],
+            claim_identifier: Permissions::TEST_CLAIM_IDENTIFIER,
+            claim_value: "hello world",
+          )
+        end
+
+        context "which the token has access to" do
+          let(:token_scopes) { [Permissions::TEST_READ_SCOPE] }
+
+          it "includes the claim in the response" do
+            get "/oidc/user_info", headers: headers
+            expect(JSON.parse(response.body)).to include(claim.claim_identifier => claim.claim_value)
+          end
+        end
+
+        context "which the token does not have access to" do
+          it "doesn't include the claim in the response" do
+            get "/oidc/user_info", headers: headers
+            expect(response.body).to_not include(claim.claim_identifier)
+          end
+        end
       end
     end
   end
