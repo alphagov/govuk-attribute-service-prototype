731455056571f505376a21c305cdbe608e611994
Michael Walker <mike@barrucadu.co.uk>
2020-08-06 09:43:18 +0100

    Add a claim IDs and permissions for the email address

--- lib/permissions.rb
+++ lib/permissions.rb
@@ -6,10 +6,18 @@ module Permissions
   # in addition, write access implies read access
   CLAIM_READ_SCOPES = {
     TEST_CLAIM_IDENTIFIER => [TEST_READ_SCOPE],
+    # email address
+    "35552825-86c7-4c4a-a9b9-7851e0ff0f7c" => %i[account_manager_access email_address_read],
+    # email address is verified?
+    "3a683bee-24a7-4ada-88af-5bfc32a40388" => %i[account_manager_access email_address_read],
   }.freeze
 
   CLAIM_WRITE_SCOPES = {
     TEST_CLAIM_IDENTIFIER => [TEST_WRITE_SCOPE],
+    # email addresss
+    "35552825-86c7-4c4a-a9b9-7851e0ff0f7c" => %i[account_manager_access],
+    # email address is verified?
+    "3a683bee-24a7-4ada-88af-5bfc32a40388" => %i[account_manager_access],
   }.freeze
 
   def self.any_of_scopes_can_read(claim, scopes)
