c6c5b2c75084dfadbd512068631e6917185f34af
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-08-17 11:12:33 +0100

    Fix column name in migration

--- db/migrate/20200814135809_use_boolean_for_email_verification_claim.rb
+++ db/migrate/20200814135809_use_boolean_for_email_verification_claim.rb
@@ -1,7 +1,7 @@
 class UseBooleanForEmailVerificationClaim < ActiveRecord::Migration[6.0]
   def up
     Claim.where(claim_identifier: "3a683bee-24a7-4ada-88af-5bfc32a40388").each do |claim|
-      claim.update!(claim_value_jsonb: claim.claim_value == "true")
+      claim.update!(claim_value: claim.claim_value == "true")
     end
   end
 
