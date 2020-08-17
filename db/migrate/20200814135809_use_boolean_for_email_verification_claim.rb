c036db4d8f7e1f9e8b081522310f25568a52cf71
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-08-17 10:12:28 +0100

    Use boolean values for email verification claim
    We are currently storing boolean values as strings, so this migration
    will replace them with actual boolean values.

---
+++ db/migrate/20200814135809_use_boolean_for_email_verification_claim.rb
@@ -0,0 +1,9 @@
+class UseBooleanForEmailVerificationClaim < ActiveRecord::Migration[6.0]
+  def up
+    Claim.where(claim_identifier: "3a683bee-24a7-4ada-88af-5bfc32a40388").each do |claim|
+      claim.update!(claim_value_jsonb: claim.claim_value == "true")
+    end
+  end
+
+  def down; end
+end
