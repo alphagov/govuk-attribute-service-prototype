ab02c7dafbf5c56bec523b7ad918005317889fa6
Michael Walker <mike@barrucadu.co.uk>
2020-10-06 14:31:05 +0100

    Implement PUT /v1/attributes/:id in terms of POST /v1/attributes

--- app/controllers/v1/attributes_controller.rb
+++ app/controllers/v1/attributes_controller.rb
@@ -17,6 +17,12 @@ class V1::AttributesController < ApplicationController
     head 400
   end
 
+  class MissingPermission < StandardError; end
+
+  rescue_from MissingPermission do
+    head 401
+  end
+
   def show
     claim_name = params.fetch(:id).to_sym
 
@@ -33,30 +39,20 @@ class V1::AttributesController < ApplicationController
 
   def update
     claim_name = params.fetch(:id).to_sym
-
-    unless Permissions.any_of_scopes_can_write(claim_name, token_scopes)
-      head 401
-      return
-    end
-
-    claim_identifier = Permissions.name_to_uuid(claim_name)
-
-    claim = Claim.upsert!(
-      subject_identifier: subject_identifier,
-      claim_identifier: claim_identifier,
-      claim_value: JSON.parse(params.fetch(:value)),
-    )
-    render json: claim.to_anonymous_hash
+    claim_value = params.fetch(:value)
+    render json: atomic_update_claims({ claim_name => claim_value }).first
   end
 
   def update_many
     claims = JSON.parse(params.fetch(:attributes)).symbolize_keys
+    render json: atomic_update_claims(claims)
+  end
 
+private
+
+  def atomic_update_claims(claims)
     all_ok = claims.all? { |claim_name, _| Permissions.any_of_scopes_can_write(claim_name, token_scopes) }
-    unless all_ok
-      head 401
-      return
-    end
+    raise MissingPermission unless all_ok
 
     upserts = Claim.transaction do
       claims.map do |claim_name, claim_value|
@@ -69,10 +65,8 @@ class V1::AttributesController < ApplicationController
       end
     end
 
-    render json: upserts.map(&:to_anonymous_hash)
+    upserts.map(&:to_anonymous_hash)
   end
-
-private
 
   def subject_identifier
     @token[:true_subject_identifier]
