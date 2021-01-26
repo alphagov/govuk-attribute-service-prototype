c446ffb6c58dd9eba432c070c8c8152437127e4c
Michael Walker <mike@barrucadu.co.uk>
2021-01-26 10:33:12 +0000

    Revert "Move reporting scope name into Permissions module"
    This reverts commit 72a8d6489cc2532c6257f42778055a3bebed0f13.

--- app/controllers/v1/report/bigquery_controller.rb
+++ app/controllers/v1/report/bigquery_controller.rb
@@ -2,7 +2,7 @@ class V1::Report::BigqueryController < ApplicationController
   before_action :authenticate_token!
 
   def create
-    head 401 and return unless @token[:scopes].include?(Permissions::REPORTING_SCOPE)
+    head 401 and return unless @token[:scopes] == %i[reporting_access]
 
     BigqueryReportExportJob.perform_later
 
